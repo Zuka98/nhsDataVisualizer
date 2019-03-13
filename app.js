cca2b6d1bdb9bbd6f1391e7ef4507e1669e7ea4e
Zuka98 <32645506+Zuka98@users.noreply.github.com>
2019-03-13 23:43:26 +0000

    123
    123

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+var app = express();
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var chalk = require('chalk');
+
+var path = require('path');
+
+var myRouter = express.Router();
+
+app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname,'/public/')));
+app.use('/css', express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname,'node_modules/jquery/dist')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// myRouter.route('/Map').get((req,res) => {
+//     res.re('map');
+// })
+
+app.use('/',myRouter);
+
+app.get('/', function(req,res){
+    res.render('index', {
+     title: 'MyLibrary',
+     nav: [{link: '/Map',  title: 'Map'},
+           {link: '/authors',  title:'Authors'}]
+    });
+}); 
+
+app.listen(3000, function(){
+    console.log(`listening on ${chalk.green('port 3000')}`);
+});
